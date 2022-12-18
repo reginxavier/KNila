0ea0c63ca7f50c4818a6dbe6a9f2fc5ddc3386cc
reginxavier <40997949+reginxavier@users.noreply.github.com>
2022-12-18 22:10:37 +0530

    Add files via upload

---
+++ KNila_DotNetCore_WebAPI/KNila_DotNetCore_WebAPI/Controllers/ContactDetailsController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace KNila_DotNetCore_WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ContactDetailsController : ControllerBase
+    {
+        private SqlConnection con;
+        private readonly IConfiguration _configuration;
+        
+        public ContactDetailsController (IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private void connection()
+        {
+            string constr = _configuration.GetConnectionString("getconn");
+            con = new SqlConnection(constr);
+        }
+
+        /// <summary>
+        /// Get Contact Details
+        /// </summary>
+        /// <returns>Contact Details List</returns>
+        [HttpGet]
+        public IEnumerable<ContactModel> GetContactDetails()
+        {
+            List<ContactModel> contact = new List<ContactModel>();
+            connection();
+            con.Open();
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM ContactDetails", con);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            DataTable dt = ds.Tables[0];
+            sda.Dispose();
+
+            foreach(DataRow dr in dt.Rows)
+            {
+                ContactModel cm = new ContactModel
+                {
+                    FirstName = dr["FirstName"].ToString(),
+                    LastName = dr["LastName"].ToString(),
+                    Email = dr["Email"].ToString(),
+                    PhoneNumber = dr["PhoneNumber"].ToString(),
+                    Address = dr["Address"].ToString(),
+                    City = dr["City"].ToString(),
+                    State = dr["State"].ToString(),
+                    Country = dr["Country"].ToString(),
+                    PostalCode = dr["PosalCode"].ToString()
+                };
+                contact.Add(cm);
+            }
+            con.Close();
+            return contact;
+        }
+
+        /// <summary>
+        /// Save Edi Details
+        /// </summary>
+        /// <param name="editContactDetails"></param>
+        /// <returns>Success/Failure</returns>
+        [HttpPost]
+        public string SaveEditDetails (ContactModel editContactDetails)
+        {
+            connection();
+            SqlCommand com = new SqlCommand("UpdateContactDetails", con);
+            com.CommandType = System.Data.CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@FirstName", editContactDetails.FirstName ?? "");
+            com.Parameters.AddWithValue("@LastName", editContactDetails.LastName ?? "");
+            com.Parameters.AddWithValue("@Email", editContactDetails.Email ?? "");
+            com.Parameters.AddWithValue("@PhoneNumber", editContactDetails.PhoneNumber ?? "");
+            com.Parameters.AddWithValue("@Address", editContactDetails.Address ?? "");
+            com.Parameters.AddWithValue("@City", editContactDetails.City ?? "");
+            com.Parameters.AddWithValue("@State", editContactDetails.State ?? "");
+            com.Parameters.AddWithValue("@Country", editContactDetails.Country ?? "");
+            com.Parameters.AddWithValue("@PostalCode", editContactDetails.PostalCode ?? "");
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+                return "Success";
+            }
+            else
+            {
+                return "Failure";
+            }
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,21 @@
+Application Requirements
+==================================================
+Microsoft SQL Server (2014)
+DB Server Name: LAPTOP-UJAFRJPR\SQLEXPRESS
+Database Name: KNila
+Authentication: Windows
+
+Visual Studio (2017)
+ASP.NET Core 2.1  - Framework (Both Web Application and API)
+==================================================
+
+Run the API Solution first - KNila_DotNetCore_WebAPI
+Run the Web Application second - KNila_DotNetCore_WebApplication
+
+Home Page will be loaded.
+Click Add New Contact link to add new contact details.
+Click Edit to edit the details of the already added contact.
+Click Back To Home Page or Go Back will redirect you to the Home Page.
+
+Select * From ContactDetails - Query to confirm the data entered in DB.
+
