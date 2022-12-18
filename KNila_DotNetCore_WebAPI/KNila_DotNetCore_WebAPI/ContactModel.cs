0c6399b218cc1e06d63601ade1bb8e3b7f34ff95
reginxavier <40997949+reginxavier@users.noreply.github.com>
2022-12-18 17:36:23 +0530

    KNila_

---
+++ KNila_DotNetCore_WebAPI/KNila_DotNetCore_WebAPI/ContactModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KNila_DotNetCore_WebAPI
+{
+    public class ContactModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
+
+        public string PostalCode { get; set; }
+    }
+}
